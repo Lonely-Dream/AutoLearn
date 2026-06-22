46971dcb8498e805325ed5644a745021330ffe06
Lonely Dream <Zhenhui.Cai@foxmail.com>
2026-06-22 16:53:15 +0800

    feat: http tools

---
+++ AutoLearn/HttpUtil.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLearn
+{
+    internal static class HttpUtil
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        public static async Task<T?> GetAsync<T>(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string jsonString = await response.Content.ReadAsStringAsync();
+
+                // 反序列化
+                return JsonConvert.DeserializeObject<T>(jsonString);
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"[HttpUtil]HTTP GET request to {url} failed.", ex);
+                throw;
+            }
+        }
+    }
+}
